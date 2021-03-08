643916a1ef7211f79a244fcc5e8e84df452658d4
vadim <vadimlosenkov0729@gmail.com>
2021-03-08 10:42:30 +0300

    added services section and started adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 $(function(){
 
+  $('.top-slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<button type="button" class="slick-next slick-arrow"><img src="images/next-arrow.svg" alt=""></button>',
+    prevArrow: '<button type="button" class="slick-prev slick-arrow"><img src="images/prev-arrow.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          arrows: false,
+        }
+      }
+    ]
+  });
+
+
 });
